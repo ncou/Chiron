af77ad14e609206acc9892735f4db52fbad2c307
ncou <nicolas_courade@yahoo.fr>
2018-05-09 18:04:05 +0200

    add a requestId Middleware + some tests

---
+++ src/Chiron/Middleware/RequestUuidMiddleware.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chiron\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UriInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class RequestUuidMiddleware implements MiddlewareInterface
+{
+
+    /**
+     * Add a unique ID for each HTTP request.
+     *
+     * @param ServerRequestInterface  $request request
+     * @param RequestHandlerInterface $handler
+     *
+     * @return object ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $uuid = $request->getHeader('X-Request-Id');
+        if (empty($uuid)) {
+            $uuid = self::uuid();
+            $request = $request->withHeader('X-Request-Id', $uuid);
+        }
+
+        $response = $handler->handle($request);
+        $response = $response->withHeader('X-Request-Id', $uuid);
+
+        return $response;
+    }
+
+    /**
+     * Generates a v4 random UUID (Universally Unique IDentifier).
+     *
+     * The version 4 UUID is purely random (except the version).
+     * It doesn't contain meaningful information such as MAC address, time, etc.
+     *
+     * See RFC 4122 for details of UUID.
+     *
+     * @return string
+     */
+    public static function uuid(): string
+    {
+        $bytes = unpack("v*", random_bytes(16));
+        $bytes[4] = $bytes[4] & 0x0fff | 0x4000;
+        $bytes[5] = $bytes[5] & 0x3fff | 0x8000;
+        return vsprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x', $bytes);
+    }
+}
---
+++ tests/Middleware/RequestUuidMiddlewareTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chiron\Tests\Middleware;
+
+use Chiron\Http\Factory\ServerRequestFactory;
+use Chiron\Http\Response;
+use Chiron\Middleware\RequestUuidMiddleware;
+use Chiron\Tests\Utils\HandlerProxy2;
+use PHPUnit\Framework\TestCase;
+
+class RequestUuidMiddlewareTest extends TestCase
+{
+    private $middleware;
+
+    /**
+     * Setup.
+     */
+    protected function setUp()
+    {
+        $this->middleware = new RequestUuidMiddleware();
+    }
+
+    public function testRequestIdIsAnObject()
+    {
+        $this->assertThat(
+            method_exists($this->middleware, 'process'),
+            $this->isTrue(),
+            'A middleware must have a handle method.'
+        );
+    }
+    public function testRequestIdShouldBeFilledIfDoesNotExistInRequestAndResponse()
+    {
+        $request  = (new ServerRequestFactory())->createServerRequestFromArray([
+            'REQUEST_URI'            => '/',
+            'REQUEST_METHOD'         => 'GET',
+        ]);
+        $handler = function ($request) use (&$uuid) {
+            $uuid = $request->getHeaderLine('X-Request-Id');
+            $this->assertNotEmpty($uuid);
+            return new Response();
+        };
+        $response   = $this->middleware->process($request, new HandlerProxy2($handler));
+
+        $this->assertEquals(
+            $response->getHeaderLine('X-Request-Id'),
+            $uuid,
+            'The same X-Request-Id must be set in request and response.'
+        );
+    }
+    public function testPropagateRequestIdToResponseIfProvidedInRequest()
+    {
+        $request = (new ServerRequestFactory())->createServerRequestFromArray([
+            'REQUEST_URI'            => '/',
+            'REQUEST_METHOD'         => 'GET',
+        ]);
+        $request = $request->withHeader('X-Request-Id', '09226165-364a-461a-bf5c-e859d70d907e');
+        $handler = function ($request) {
+            $this->assertEquals(
+                '09226165-364a-461a-bf5c-e859d70d907e',
+                $request->getHeaderLine('X-Request-Id'),
+                'The Request header must not be modified.'
+            );
+            return new Response();
+        };
+        $response   = $this->middleware->process($request, new HandlerProxy2($handler));
+        $this->assertEquals(
+            '09226165-364a-461a-bf5c-e859d70d907e',
+            $response->getHeaderLine('X-Request-Id'),
+            'The request X-Request-ID header must be set in the response.'
+        );
+    }
+
+}
