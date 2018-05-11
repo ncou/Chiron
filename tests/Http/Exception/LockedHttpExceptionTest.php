629e1a5ecfcc73387ad323158a961bfe0dd05285
ncou <nicolas_courade@yahoo.fr>
2018-05-12 01:24:02 +0200

    update test

---
+++ tests/Http/Exception/LockedHttpExceptionTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-stratigility for the canonical source repository
+ *
+ * @copyright Copyright (c) 2016-2018 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-stratigility/blob/master/LICENSE.md New BSD License
+ */
+declare(strict_types=1);
+
+namespace Chiron\Tests\Http\Exception;
+
+use Chiron\Http\Exception\LockedHttpException;
+
+class LockedHttpExceptionTest extends HttpExceptionTest
+{
+    protected function createException()
+    {
+        return new LockedHttpException();
+    }
+}
