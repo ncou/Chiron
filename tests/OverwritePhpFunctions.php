349c70b01abaf6077e71b78b936eb4e943c79dda
ncou <nicolas_courade@yahoo.fr>
2018-05-13 02:16:43 +0200

    update

---
+++ tests/OverwritePhpFunctions.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Chiron\Middleware;
+
+use Chiron\Tests\Utils\HeaderStack;
+
+/**
+ * Have headers been sent?
+ *
+ * @param null|string $file
+ * @param null|int    $line
+ *
+ * @return bool false
+ */
+function headers_sent($file = null, $line = null): bool
+{
+    return false;
+}
+/**
+ * Emit a header, without creating actual output artifacts.
+ *
+ * @param string   $string
+ * @param bool     $replace
+ * @param null|int $statusCode
+ *
+ * @return void
+ */
+function header($string, $replace = true, $statusCode = null): void
+{
+    HeaderStack::push(
+        [
+            'header'      => $string,
+            'replace'     => $replace,
+            'status_code' => $statusCode,
+        ]
+    );
+}
