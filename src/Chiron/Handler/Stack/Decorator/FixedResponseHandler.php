d1fac9160109c1f993e384b727e7168f370c4b26
ncou <nicolas_courade@yahoo.fr>
2018-06-09 12:30:56 +0200

    update

--- src/Chiron/Handler/Stack/Decorator/FixedResponseHandler.php
+++ src/Chiron/Handler/Stack/Decorator/FixedResponseHandler.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * @see       https://github.com/zendframework/zend-stratigility for the canonical source repository
- *
- * @copyright Copyright (c) 2017-2018 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-stratigility/blob/master/LICENSE.md New BSD License
- */
 
 declare(strict_types=1);
 
@@ -31,12 +25,6 @@ class FixedResponseHandler implements RequestHandlerInterface
         $this->fixedResponse = $response;
     }
 
-    /**
-     * {@inheritdoc}
-     *
-     * @throws Exception\MissingResponseException if the decorated middleware
-     *                                            fails to produce a response
-     */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         return $this->fixedResponse;
